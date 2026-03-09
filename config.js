0a015f8a2b66927200887a8990ba34fb70befe80
dgbeglia <61236396+dgbeglia@users.noreply.github.com>
2026-03-09 08:37:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
     // Tu clave aquí está protegida porque se carga como módulo
-    GEMINI_KEY: "AIzaSyC-XkdO69drkY536m2ATzm_GID3EiZ9oH8"
+    GEMINI_KEY: "AIzaSyBFUMOLBQYq-cZrXayIZ-yb6sh4pRdwXBc"
 };
